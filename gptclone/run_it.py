2e18ebe2069bdd4a7d663bf26af42d179b5b57cd
brianthehardy <brian.the.hardy@gmail.com>
2023-05-21 17:05:59 +0200

    minor tweaks to support pythonanywhere

--- gptclone/run_it.py
+++ gptclone/run_it.py
@@ -2,11 +2,6 @@
 # Exists so I can run this on pythoneverywhere
 
 from . import create_app
-app = create_app()
-'''
-In any case, what worked was creating a module, run_it.py, inside the "mutilator" package. 
-In run_it.py, I imported create_app from the package's global namespace--so just "from mutilator import create_app", 
-and then I just added the code "app = create_app()". 
-Then my WSGI config file had "from mutilator.run_it import app as application" and it worked just fine.
 
-'''
+app = create_app()
+app.secret_key = '_5#y2L"F4Q8znxec]'
